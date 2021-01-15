0ff1acd06065b283458ec926fae7042c25a7809f
Gustavo <gzbinden@ideasa.com.ar>
2021-01-14 21:47:22 -0300

    make it better

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+//const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+//seting
+app.set('port', 4000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//middleware
+
+//routes
+app.use(require('./routes'));
+
+//static files
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//iniciando el servidor
+app.listen(app.get('port'), () => {
+    console.log('Iniciado en el puerto', app.get('port'));
+});
